921cb2a163b5474468af82b88f9b3568c963b785
Anastasiya Sergeevna <balashova-nastya@list.ru>
2020-06-18 14:29:53 +0300

    Изменение цвета лайка при нажатии

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ for (let heart of heart_list) {
   heart.addEventListener("click", function () {
     let likes_number = heart.querySelector('.element__likes-number');
 
-    if (heart.classList.contains('added')) {
+    if (heart.classList.contains('element__like_status_added')) {
       likes_number.textContent--;
     } else {
       likes_number.textContent++;
     }
 
-    heart.classList.toggle('added');
+    heart.classList.toggle('element__like_status_added');
   });
 }
