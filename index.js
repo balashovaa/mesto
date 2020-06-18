efe319e795868b1a900877f43e24287dc61078a4
Anastasiya Sergeevna <balashova-nastya@list.ru>
2020-06-18 12:26:13 +0300

    организация проекта по БЭМ

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-let heart_list = document.querySelectorAll('.elements__like');
+let heart_list = document.querySelectorAll('.element__like');
 
 
 for (let heart of heart_list) {
   heart.addEventListener("click", function () {
-    let likes_number = heart.querySelector('.elements__likes-number');
+    let likes_number = heart.querySelector('.element__likes-number');
 
     if (heart.classList.contains('added')) {
       likes_number.textContent--;
