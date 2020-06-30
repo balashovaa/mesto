654a62ace4ecb7ec98a2e742dfdc2703e31f0b75
Anastasiya Sergeevna <balashova-nastya@list.ru>
2020-06-30 12:31:54 +0300

    feat: реализация лайков

--- scripts/index.js
+++ scripts/index.js
@@ -68,3 +68,21 @@ function handleAddPhotoButtonClick() {
 }
 
 addPhotoButton.addEventListener('click', handleAddPhotoButtonClick);
+
+
+let heart_list = document.querySelectorAll('.element__like');
+
+
+for (let heart of heart_list) {
+  heart.addEventListener("click", function () {
+    let likes_number = heart.querySelector('.element__likes-number');
+
+    if (heart.classList.contains('element__like_status_added')) {
+      likes_number.textContent--;
+    } else {
+      likes_number.textContent++;
+    }
+
+    heart.classList.toggle('element__like_status_added');
+  });
+}
